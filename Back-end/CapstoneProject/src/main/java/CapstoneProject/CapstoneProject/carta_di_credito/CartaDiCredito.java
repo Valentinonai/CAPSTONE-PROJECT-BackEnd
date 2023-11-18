9f2ff210a94cd2924530d94b1edf89f4f2514de6
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-18 14:29:14 +0100

    creata entità carta di credito

---
+++ Back-end/CapstoneProject/src/main/java/CapstoneProject/CapstoneProject/carta_di_credito/CartaDiCredito.java
@@ -0,0 +1,47 @@
+package CapstoneProject.CapstoneProject.carta_di_credito;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "carte_di_credito")
+public class CartaDiCredito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "numero_carta",nullable = false)
+    private String numero_carta;
+    @Column(name = "cvv",nullable = false)
+    private String cvv;
+    @Column(name = "data_di_scadenza",nullable = false)
+    private String data_di_scadenza;
+    @Column(name = "data_creazione")
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date data_creazione;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    //metodi e costruttori
+
+    public CartaDiCredito(){}
+
+    public CartaDiCredito(String numero_carta, String cvv, String data_di_scadenza, User user) {
+        this.numero_carta = numero_carta;
+        this.cvv = cvv;
+        this.data_di_scadenza = data_di_scadenza;
+        this.user = user;
+    }
+}
--- Back-end/CapstoneProject/src/main/java/CapstoneProject/CapstoneProject/item/Item.java
+++ Back-end/CapstoneProject/src/main/java/CapstoneProject/CapstoneProject/item/Item.java
@@ -18,26 +18,36 @@ public abstract class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected long id;
+
     @Column(name = "marca", nullable = false)
     protected String marca;
+
     @Column(name = "nome", nullable = false)
     protected String nome;
+
     @Column(name = "descizione")
     protected String descrizione;
+
     @Column(name = "prezzo", nullable = false)
     protected double prezzo;
+
     @Column(name = "immagine_url")
     protected String immagineUrl = "https://res.cloudinary.com/dzr77mvcs/image/upload/v1699804334/uzqt1xnviwyjlcxqnqka.webp";
+
     @Column(name = "data_creazione")
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     protected Date data_creazione;
+
     @Column(name = "data_di_rilascio", nullable = false)
     protected LocalDate data_di_rilascio;
+
     @Column(name = "potenza_di_picco",nullable = false)
     protected int potenza_di_picco;
+
     @Column(name = "quantità",nullable = false)
     protected int quantità;
+
     @Column(name = "categoria",nullable = false)
     @Enumerated(EnumType.STRING)
     protected Categoria categoria;

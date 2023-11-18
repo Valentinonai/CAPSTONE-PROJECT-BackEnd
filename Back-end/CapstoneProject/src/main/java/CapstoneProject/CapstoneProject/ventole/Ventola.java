df7d4358adf2d2ee4afcd6f6b244e2ea885a56d8
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-18 14:18:51 +0100

    creata entità ventole

---
+++ Back-end/CapstoneProject/src/main/java/CapstoneProject/CapstoneProject/ventole/Ventola.java
@@ -0,0 +1,40 @@
+package CapstoneProject.CapstoneProject.ventole;
+
+import CapstoneProject.CapstoneProject.Enum.Categoria;
+import CapstoneProject.CapstoneProject.item.Item;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Builder
+@Entity
+@Table(name = "ventole")
+public class Ventola extends Item {
+    @Column(name = "rpm_min")
+    private int rpm_min;
+    @Column(name = "rpm_max")
+    private int rpm_max;
+    @Column(name = "controllo_pwm")
+    private boolean pwm;
+    @Column(name = "dimensione",nullable = false)
+    private int dimensione;
+
+    //metodi e costruttori
+
+    public Ventola(){}
+
+    public Ventola(String marca, String nome, String descrizione, double prezzo, LocalDate data_di_rilascio, int potenza_di_picco, int quantità, Categoria categoria, int rpm_min, int rpm_max, boolean pwm, int dimensione) {
+        super(marca, nome, descrizione, prezzo, data_di_rilascio, potenza_di_picco, quantità, categoria);
+        this.rpm_min = rpm_min;
+        this.rpm_max = rpm_max;
+        this.pwm = pwm;
+        this.dimensione = dimensione;
+    }
+}

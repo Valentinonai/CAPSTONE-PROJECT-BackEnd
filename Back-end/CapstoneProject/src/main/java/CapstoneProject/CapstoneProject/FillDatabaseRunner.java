71655720bb6db410195a431f7774124536afe377
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-27 10:25:03 +0100

    create Ram a database

--- Back-end/CapstoneProject/src/main/java/CapstoneProject/CapstoneProject/FillDatabaseRunner.java
+++ Back-end/CapstoneProject/src/main/java/CapstoneProject/CapstoneProject/FillDatabaseRunner.java
@@ -1,14 +1,19 @@
 package CapstoneProject.CapstoneProject;
 
+import CapstoneProject.CapstoneProject.Enum.Categoria;
 import CapstoneProject.CapstoneProject.Enum.Formato;
 import CapstoneProject.CapstoneProject.cpu.CpuPayLoad;
 import CapstoneProject.CapstoneProject.item.ItemService;
+import CapstoneProject.CapstoneProject.ram.Ram;
+import CapstoneProject.CapstoneProject.ram.RamPayLoad;
 import CapstoneProject.CapstoneProject.scheda_madre.SchedaMadrePayLoad;
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 @Component
 public class FillDatabaseRunner implements CommandLineRunner {
@@ -19,7 +24,7 @@ public class FillDatabaseRunner implements CommandLineRunner {
 
         //---------------SCHEDA MADRE--------------------
 
-       /* itemService.saveSchedaMadre(new SchedaMadrePayLoad("Asus","Rog Strix Z790-E","Stile e sostanza si uniscono come mai prima d'ora nella ROG Strix Z790-E II, ricca di funzionalità.",669, LocalDate.of(2023,11,01),120,100, "ATX","Intel Z790","Intel LGA 1700","DDR5",192,true,true,12,3,true));
+       /*itemService.saveSchedaMadre(new SchedaMadrePayLoad("Asus","Rog Strix Z790-E","Stile e sostanza si uniscono come mai prima d'ora nella ROG Strix Z790-E II, ricca di funzionalità.",669, LocalDate.of(2023,11,01),120,100, "ATX","Intel Z790","Intel LGA 1700","DDR5",192,true,true,12,3,true));
 
         itemService.saveSchedaMadre(new SchedaMadrePayLoad("Asus","ROG MAXIMUS Z790 DARK HERO","ROG Maximus Z790 Dark Hero vanta un potente raffreddamento, WiFi 7 e un ampio slot PCIe 5.0 per prestazioni impareggiabili.",869, LocalDate.of(2023,11,01),120,20, "ATX","Intel Z790","Intel LGA 1700","DDR5",192,true,true,21,3,true));
         itemService.saveSchedaMadre(new SchedaMadrePayLoad("Asus","ROG STRIX Z790-I GAMING WIFI","Le regole sono fatte per essere infrante.",569, LocalDate.of(2023,11,01),120,10, "MINI_ITX","Intel Z790","Intel LGA 1700","DDR5",96,true,true,12,1,true));
@@ -36,11 +41,11 @@ public class FillDatabaseRunner implements CommandLineRunner {
 
         itemService.saveSchedaMadre(new SchedaMadrePayLoad("MSI","MPG B650 EDGE WIFI","MPG B650 EDGE WIFI utilizza una combinazione di colori bianco argentato per mostrare personalità e identità diverse, unendo l'eleganza alla potenza.",240, LocalDate.of(2023,11,01),120,5, "ATX", "AMD X670","AMD AM5","DDR5",192,true,true,19,3,true));
 
-        itemService.saveSchedaMadre(new SchedaMadrePayLoad("MSI","MEG X670E ACE","La serie MEG si rivela come una leggenda che rappresenta la massima eleganza nel gaming.",938, LocalDate.of(2023,11,01),120,5, "ATX", "AMD X670","AMD AM5","DDR5",192,true,true,21,3,true));*/
+        itemService.saveSchedaMadre(new SchedaMadrePayLoad("MSI","MEG X670E ACE","La serie MEG si rivela come una leggenda che rappresenta la massima eleganza nel gaming.",938, LocalDate.of(2023,11,01),120,5, "ATX", "AMD X670","AMD AM5","DDR5",192,true,true,21,3,true));
 
         //------------------------------CPU----------------------------------------
 
-      /*  itemService.saveCpu(new CpuPayLoad("Intel","Intel® Core™ i7 processor 14700K",
+       itemService.saveCpu(new CpuPayLoad("Intel","Intel® Core™ i7 processor 14700K",
                 "Intel® Core™ i7 Processors (14th gen)",430,LocalDate.of(2023,10,15),253,10,"Intel LGA 1700",20,28,"5.6GHz",28,33,100,true,"DDR4/DDR5"));
 
         itemService.saveCpu(new CpuPayLoad("Intel","Intel® Core™ i9 processor 14900K",
@@ -68,8 +73,21 @@ public class FillDatabaseRunner implements CommandLineRunner {
                                                 "AMD Ryzen™ 9 Desktop Processors",654,LocalDate.of(2023,2,23),120,10,"AMD AM5",16,32,"5.7GHz",16,128,89,true,"DDR4/DDR5"));
 
         itemService.saveCpu(new CpuPayLoad("AMD Ryzen","AMD RYZEN™ 7 5800X3D Processor",
-                                                "AMD Ryzen™ 7 Desktop Processors",392,LocalDate.of(2020,11,5),105,10,"AMD AM4",8,16,"4.5GHz",4,32,90,false,"DDR4"));*/
+                                                "AMD Ryzen™ 7 Desktop Processors",392,LocalDate.of(2020,11,5),105,10,"AMD AM4",8,16,"4.5GHz",4,32,90,false,"DDR4"));
 
 
+        //--------------------------------------RAM--------------------------------------------
+
+       itemService.saveRam(new RamPayLoad("Corsair","DRAM DOMINATOR PLATINUM RGB 2X16Gb ","Spingi le prestazioni oltre ogni limite con la memoria DDR5 CORSAIR DOMINATOR PLATINUM RGB ottimizzata per i processori Intel.",234,LocalDate.of(2022,10,10),6,20,"DDR5",7200,32, Arrays.asList(1L,2L,3L,4L,7L,8L)));
+
+       itemService.saveRam(new RamPayLoad("Corsair","DRAM DOMINATOR PLATINUM RGB ","Spingi le prestazioni oltre ogni limite con la memoria DDR5 CORSAIR DOMINATOR PLATINUM RGB ottimizzata per i processori Intel.",194,LocalDate.of(2022,10,10),6,20,"DDR5",6000,32, Arrays.asList(5L,6L,9L,10L)));
+
+        itemService.saveRam(new RamPayLoad("Corsair","VENGEANCE® RGB 32GB (2x16GB) DDR5 DRAM 6000MT/s CL30 Memory Kit — Black","CORSAIR VENGEANCE RGB DDR5 memory delivers DDR5 performance, higher frequencies, and greater capacities.",139,LocalDate.of(2021,10,10),6,10,"DDR5",6000,32, Arrays.asList(1L,2L,3L,4L,7L,8L)));
+
+        itemService.saveRam(new RamPayLoad("Corsair","Kit di memoria DDR5 DRAM VENGEANCE RGB 32GB (2x16GB) 6000MT/s C30, Kit di memoria AMD EXPO ","Raggiungi il massimo delle prestazioni RAM DDR5 ottimizzate per le schede madre AMD, con RGB a dieci zone per memoria DDR5 per illuminare il tuo sistema PC all'avanguardia.",139,LocalDate.of(2022,10,10),6,20,"DDR5",4800,32, Arrays.asList(5L,6L,9L,10L)));
+
+        itemService.saveRam(new RamPayLoad("Corsair","DRAM DOMINATOR TITANIUM RGB 32 GB ","La memoria DDR5 CORSAIR DOMINATOR TITANIUM RGB unisce un design pulito e raffinato ad una struttura in alluminio pressofuso di qualità superiore e un sistema di illuminazione all’avanguardia.",244.99,LocalDate.of(2023,10,10),6,10,"DDR5",4800,32, Arrays.asList(1L,2L,3L,4L,7L,8L)));
+
+        itemService.saveRam(new RamPayLoad("Corsair","Kit di memoria DDR5 DRAM DOMINATOR TITANIUM RGB 32 GB (2x16 GB) 6000 MT/s CL30 AMD EXPO – Grey ","La memoria DDR5 CORSAIR DOMINATOR TITANIUM RGB unisce un design pulito e raffinato ad una struttura in alluminio pressofuso di qualità superiore e un sistema di illuminazione all’avanguardia.",204.99,LocalDate.of(2023,10,10),6,20,"DDR5",4800,32, Arrays.asList(5L,6L,9L,10L)));*/
     }
 }

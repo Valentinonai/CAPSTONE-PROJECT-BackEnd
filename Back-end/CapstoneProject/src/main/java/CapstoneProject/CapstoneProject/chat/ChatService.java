c413ba89b3f9f8bc0d122da6c7fbf181ce6f00d1
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-12-07 23:40:41 +0100

    cambiato endpoint

--- Back-end/CapstoneProject/src/main/java/CapstoneProject/CapstoneProject/chat/ChatService.java
+++ Back-end/CapstoneProject/src/main/java/CapstoneProject/CapstoneProject/chat/ChatService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -17,44 +18,56 @@ import java.net.URL;
 public class ChatService {
     @Value("${openai.api.key}")
     private String key;
+
     public ChatPayload domanda(ChatPayload message) throws IOException {
-        String url = "https://api.openai.com/v1/completions";
-        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
 
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Authorization", "Bearer "+key);
+        String url = "https://api.openai.com/v1/chat/completions";
+        String apiKey = key;
+        String model = "gpt-3.5-turbo";
 
-        JSONObject data = new JSONObject();
-        data.put("model", "text-davinci-003");
-        data.put("prompt", message.messaggio());
-        data.put("max_tokens", 4000);
-        data.put("temperature", 1.0);
-
-        con.setDoOutput(true);
-        con.getOutputStream().write(data.toString().getBytes());
-
-        String output = new BufferedReader(new InputStreamReader(con.getInputStream())).lines()
-                .reduce((a, b) -> a + b).get();
-
-        JSONObject jsonObject = new JSONObject(output);
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+            connection.setRequestProperty("Content-Type", "application/json");
 
 
-        JSONArray choicesArray = jsonObject.getJSONArray("choices");
+            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + message.messaggio() + "\"}]}";
+            connection.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(body);
+            writer.flush();
+            writer.close();
 
 
-        JSONObject firstChoice = choicesArray.getJSONObject(0);
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+
+            StringBuffer response = new StringBuffer();
+
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+            br.close();
 
 
-        String str = firstChoice.getString("text");
-        
-        int secondaRigaIndex = str.indexOf("\n", str.indexOf("\n") + 1);
+            return new ChatPayload(extractMessageFromJSONResponse(response.toString()));
 
-        if (secondaRigaIndex != -1) {
-        return new ChatPayload(str.substring(secondaRigaIndex + 1));
-
-    }else throw new SingleBadRequest("Errore");
-
-
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
+
+
+        public  String extractMessageFromJSONResponse(String response) {
+            int start = response.indexOf("content")+ 11;
+
+            int end = response.indexOf("\"", start);
+
+            return response.substring(start, end);
+
+        }
+    }
+
+
